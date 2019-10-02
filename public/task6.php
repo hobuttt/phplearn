4bf4ee5cb3bc7cd9c3c6d9b0a2323b014c50d54a
hobutt <hobuttt.ne@gmail.ru>
2019-10-02 20:45:09 +0300

    Task 6 complite

--- public/task6.php
+++ public/task6.php
@@ -1,5 +1,18 @@
 <?php
     $studentsCount = rand(1, 1000000);
-    echo ($studentsCount). '<br />';
-    echo ($studentsCount). '<br />';
+    $studentsDuoLastNum = substr($studentsCount, -2, 2);
+    $studentsLastNum = substr($studentsCount, -1, 1);
+
+    switch (true) {
+        case $studentsDuoLastNum >= 10 && $studentsDuoLastNum < 21: echo "на учебе $studentsCount студентов";
+            break;
+        case $studentsLastNum > 1 && $studentsLastNum < 5: echo "на учебе $studentsCount студента";
+            break;
+        case $studentsLastNum > 4 && $studentsLastNum < 10: echo "на учебе $studentsCount студентов";
+            break;
+        case $studentsLastNum > 0 && $studentsLastNum < 2: echo "на учебе $studentsCount студент";
+            break;
+        default: echo "на учебе $studentsCount студентов";
+            break;
+    }
 
