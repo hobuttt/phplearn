f053286c9c48f19700096b1e6b056fd58fc78997
hobutt <Ghjujy27>
2019-10-03 14:09:56 +0300

    TEst 2

--- public/task5.php
+++ public/task5.php
@@ -1,8 +1,14 @@
 <?php
     $city1 = 10;
-    $city2 = 30;
+    $city2 = 50;
     $city1Radius = 7;
     $city2Radius = 11;
-    $citySpeed = 70;
-    $highwaySpeed = 90;
-
+    $pointSCity1 = $city1 - $city1Radius;
+    $pointNCity1 = $city1 + $city1Radius;
+    $pointSCity2 = $city2 - $city1Radius;
+    $pointNCity2 = $city2 + $city1Radius;
+    for ($i = 0; $i < 10; $i++) {
+        $car = rand(0, 100);
+        $speed = ($car < $pointSCity1 && $car > $pointNCity1) || ($car < $pointSCity2 && $car > $pointNCity2) ? 90 : 70;
+        echo "Машина 4 едет по городу на $car км со скоростью $speed <br />";
+    }
