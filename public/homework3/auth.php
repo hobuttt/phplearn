4b71b476a3c2dca95c8488038deb854849d6f189
hobutt <Ghjujy27>
2019-10-16 08:32:34 +0300

    homework 310

--- public/homework3/auth.php
+++ public/homework3/auth.php
@@ -1,7 +1,8 @@
 <?php
     $login = 'root';
     $password = 'admin';
-    function auth($getLogin, $getPassword) {
+    function auth($getLogin, $getPassword, $login, $password) {
+        var_dump($getLogin, $getPassword, $login, $password);
         if ($getLogin === $login && $getPassword === $password) {
             return true;
         }
