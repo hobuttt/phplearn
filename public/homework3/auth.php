5046cab55f3bedf21a9bd46697e5ed0c6cb4e2f7
hobutt <Ghjujy27>
2019-10-17 08:52:09 +0300

    homework 311

--- public/homework3/auth.php
+++ public/homework3/auth.php
@@ -1,9 +1,9 @@
 <?php
-    $login = 'root';
-    $password = 'admin';
-    function auth($getLogin, $getPassword, $login, $password) {
-        var_dump($getLogin, $getPassword, $login, $password);
-        if ($getLogin === $login && $getPassword === $password) {
+function auth(string $getLogin, string $getPassword) {
+    $users = require './bd/logins.php';
+    foreach ($users as $user) {
+        if($user['login'] == $getLogin && $user['password'] == $getPassword) {
             return true;
         }
     }
+}
